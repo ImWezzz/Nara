45897960dec7a01b5910992da0788282074b963f
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-23 21:59:34 -0300

    Update stats.js

--- commands/Information/stats.js
+++ commands/Information/stats.js
@@ -11,20 +11,20 @@ const body = {
       let commands = ctx.bot.skyfold.commands.HybridType.size;
       let users = ctx.bot.guilds.cache.map(x => x.memberCount ?? 0).reduce((a, b) => a + b, 0);
       let e = new EmbedBuilder()
-      .setTitle(`${ctx.bot.user.username} stats`)
+      .setTitle(`${ctx.bot.user.username}'s stats`)
       .setDescription(`
-**• Bot Stats**
+**<:Nara:1086814524440584373> Bot Stats**
 
-**Servers:** ${ctx.bot.guilds.cache.size}
-**Users:** ${users}
-**Commands:** ${commands} 
-**Ping:** ${ctx.bot.ws.ping}ms
+> Servers: ${ctx.bot.guilds.cache.size}
+> Users: ${users}
+> Commands: ${commands}
+> Ping: ${ctx.bot.ws.ping}ms
 
-**• Development**
+**<:Development:1086805736618737716> Development**
 
-**Language:** JavaScript
-**Library:** Discord.JS v${require('discord.js').version}
-**Package:** Erine v${require('erine').version}
+> Language: <:JavaScript:1086800160039899277>  JavaScript
+> Library: <:DiscordJS:1086800106742874155>  Discord.JS v${require('discord.js').version}
+> Framework: <:Erine:1086800143170420766> Erine v${require('erine').version}
 
 `)
       .setColor('dcdcdc')

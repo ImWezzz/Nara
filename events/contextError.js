6bc7b024b12a0bc6ea599d026567a7fe79969a54
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-03-24 00:51:46 -0300

    Update contextError.js

--- events/contextError.js
+++ events/contextError.js
@@ -1,19 +1,31 @@
 const { ErineClient, Errors } = require("erine");
 
 const event = {
-  name: 'contextError',
-  /**
+    name: 'contextError',
+    /**
   * @param {Error} client
   */
-  async code(error) {
-      if(error instanceof Errors.CommandNotFound) return
-      else if(error instanceof Errors.MissingRequiredParam) {
+    async code(error) {
+        if(error instanceof Errors.CommandNotFound) return
+        else if(error instanceof Errors.MissingRequiredParam) {
           return error.ctx.send(`Missing parameters: **${error.param.name}**`)
-      }
-      else if(error instanceof Errors.InvalidParamMember) {
-          return error.ctx.send("User not found")
-      }
-  }
+        }
+        else if(error instanceof Errors.InvalidParamMember) {
+            return error.ctx.send("User not found")
+        }
+        else if(error instanceof Errors.InvalidParamChannel) {
+            return error.ctx.send("Channel not found")
+        }
+        else if(error instanceof Errors.InvalidParamRole) {
+            return error.ctx.send("Role not found")
+        }
+        else if(error instanceof Errors.MissingPermission) {
+          return error.ctx.send(`Missing **${error.permissions}** permissions`)
+        }
+        else if(error instanceof Errors.MissingBotPermission) {
+        return error.ctx.send(`I missing **${error.permissions}** permissions`)
+        }
+    }
 }
 
 

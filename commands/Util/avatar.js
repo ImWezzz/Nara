23a57b7265d5f0490b50e9a919cbe76cca55f522
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-04-10 01:42:04 -0300

    Update avatar.js

--- commands/Util/avatar.js
+++ commands/Util/avatar.js
@@ -2,20 +2,20 @@ const { HybridBuilder, ParamsBuilder } = require('erine');
 const { EmbedBuilder } = require("discord.js");
 
 const body = {
-   data: new HybridBuilder()
-   .setName('avatar')
-   .setAliases('pfp')
-   .setDescription('Get a user avatar'),
-  params: new ParamsBuilder()
-  .addMember({ name: "user", description: "The user", required: false }),
-  async code(ctx) {
+    data: new HybridBuilder()
+    .setName('avatar')
+    .setAliases('pfp')
+    .setDescription('Muestra el avatar de un usuario'),
+    params: new ParamsBuilder()
+    .addMember({ name: "user", description: "TEl usuario", required: false }),
+    async code(ctx) {
     const u = ctx.get("user")?.user || ctx.author
     const embed = new EmbedBuilder()
-      .setDescription(`${u.username} avatar`)
-      .setImage(u.displayAvatarURL({ size: 1024 }))
-      .setColor('dcdcdc')
-  await ctx.send({ embeds: [embed] })
-  }
+        .setDescription(`Avatar de ${u.username}`)
+        .setImage(u.displayAvatarURL({ size: 1024 }))
+        .setColor('dcdcdc')
+    await ctx.send({ embeds: [embed] })
+    }
 }
 
 module.exports = { body }

12ab81c62454b522e07c49bbe8c8e73a4cfe4135
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-04-10 01:43:49 -0300

    Update invite.js

--- commands/Util/invite.js
+++ commands/Util/invite.js
@@ -4,12 +4,12 @@ const { EmbedBuilder } = require('discord.js')
 const body = {
     data: new HybridBuilder()
     .setName('invite')
-    .setDescription('Invite me for ours servers'),
+    .setDescription('Obten mi invitación'),
     async code(ctx) {
     const e = new EmbedBuilder()
-    .setTitle('Nara invitation')
+    .setTitle('Invitación')
     .setColor('dcdcdc')
-    .setDescription('Here is my invitation \n All Perms: [Click here](https://discord.com/api/oauth2/authorize?client_id=977278077539323984&permissions=8&scope=bot) \n Recommended permissions: [Click here](https://discord.com/api/oauth2/authorize?client_id=977278077539323984&permissions=1377409825911&scope=bot)')
+    .setDescription('Aquí tienes mi invitación \n Todos los permisos: [Click here](https://discord.com/api/oauth2/authorize?client_id=977278077539323984&permissions=8&scope=bot) \n Permisos recomendados: [Click here](https://discord.com/api/oauth2/authorize?client_id=977278077539323984&permissions=1377409825911&scope=bot)')
     ctx.send({ embeds: [e] })
     }
 }

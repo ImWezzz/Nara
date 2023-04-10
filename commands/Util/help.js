ea44f1dd4a6a667a1cf6ed509b38927935027d56
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-04-10 01:49:50 -0300

    Update help.js

--- commands/Util/help.js
+++ commands/Util/help.js
@@ -5,32 +5,32 @@ const body = {
     data: new HybridBuilder()
     .setName('help')
     .setAliases('h','help')
-    .setDescription('Help menu'),
+    .setDescription('Muestra mi lista de comandos'),
     
     async code(d) {
-       let icono = d.guild.iconURL()
+        let icono = d.guild.iconURL()
     if (!icono) {
     icono = d.user.displayAvatarURL()
-  }
-    let serverinfo = new EmbedBuilder()
-   
-      .setTitle(`Nara Commands`) 
+    }
+    let help = new EmbedBuilder()
     
-      .setDescription(`
-Hello ${d.author} this is my list of commands.
+        .setTitle(`Lista de comandos`)     
+        .setDescription(`
+Hola ${d.author} aquí tienes mi lista de comandos
 
-**Information**
-userinfo・serverinfo
+> Administración
+setprefix 
 
-**Moderation/Administration**
-setprefix
+> Información
+serverinfo  •  stats  •  userinfo
 
-**Tools**
-8ball・avatar・say・ping・invite`)
+> Utilidad
+8ball  •  avatar  •  icon  •  invite  •  ping  •  say
+        `)
     .setColor('dcdcdc')
     .setThumbnail(icono)
     d.send({
-      embeds: [serverinfo]
+        embeds: [help]
     })
     }
 }

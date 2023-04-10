85fd91c127bac9d02cecf601241842da6ffc0154
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-04-10 01:43:27 -0300

    Update icon.js

--- commands/Util/icon.js
+++ commands/Util/icon.js
@@ -2,22 +2,22 @@ const { HybridBuilder, Context } = require('erine');
 const { EmbedBuilder } = require('discord.js');
 
 const body = {
-  data: new HybridBuilder()
-  .setName('servericon')
-  .setAliases("icon")
-  .setDescription('Show the server icon'),
+    data: new HybridBuilder()
+    .setName('servericon')
+    .setAliases("icon")
+    .setDescription('Muestra el icono del servidor'),
     /**
     @param {Context} ctx
     */
-  async code(ctx) {
+        async code(ctx) {
     let img = ctx.bot.guilds.cache.get(ctx.guild.id).iconURL({ size: 1024 })
     const e = new EmbedBuilder()
     
-    .setTitle(`${ctx.guild.name} icon`)
-    .setColor('White')
+    .setTitle(`Icono de${ctx.guild.name}`)
+    .setColor('dcdcdc')
     .setImage(img)
     await ctx.send({ embeds: [e] })
-  }
+    }
 }
 
 module.exports = { body }

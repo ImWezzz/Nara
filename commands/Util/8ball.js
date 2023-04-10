ff72815f165b6acc3fd06122972943de1919387f
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-04-10 01:41:13 -0300

    Update 8ball.js

--- commands/Util/8ball.js
+++ commands/Util/8ball.js
@@ -1,16 +1,16 @@
 const { HybridBuilder, ParamsBuilder } = require("erine");
 
 const body = {
-     data: new HybridBuilder()
-     .setName('8ball')
-     .setDescription('Write a question '),
-     params: new ParamsBuilder() 
-     .addString({ name: "question", description: "Question", required: true}),
-     async code(ctx) {
-       const question = ["Yes", "No", "idk", "Maybe", "I don `t believe", "Yes I believe", "Ask again", "What?", "I have a little lag, ask again", "If you put your mind to it, yes", "You say?"]
-        const random = question[Math.floor(Math.random() * question.length)]
+        data: new HybridBuilder()
+        .setName('8ball')
+        .setDescription('Preguntale algo a la bola magica'),
+        params: new ParamsBuilder() 
+        .addString({ name: "pregunta", description: "Pregunta", required: true}),
+        async code(ctx) {
+        const pregunta = ["Si", "No", "Tu crees?", "Tal vez", "No creo", "Si crees, si", "Posiblemente", "Posiblemente no", "Quien sabe", "No sé", "Habria que demostrarlo", "Efectivamente", "Por supuesto!", "Estoy más que segura", "Sii!", "Vamos :D"]
+        const random = pregunta[Math.floor(Math.random() * pregunta.length)]
         await ctx.send(`\n:8ball: ${random}`)
-     }
+        }
 }
 
 module.exports = { body }

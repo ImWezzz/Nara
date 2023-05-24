6f4b1af5765b533a24c79b8a395734590eb251fe
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-05-24 01:13:52 -0300

    Update stats.js

--- commands/Information/stats.js
+++ commands/Information/stats.js
@@ -13,26 +13,26 @@ const body = {
       let e = new EmbedBuilder()
       .setTitle(`Estadisticas de ${ctx.bot.user.username}`)
       .setDescription(`
-**<:Nara:1086814524440584373> Bot Stats**
+**Bot Stats**
 
 > Servidores: ${ctx.bot.guilds.cache.size}
 > Usuarios: ${users}
 > Comandos: ${commands}
 > Ping: ${ctx.bot.ws.ping}ms
 
-**<:Development:1086805736618737716> Desarollo**
+**Desarollo**
 
-> <:Developer:1086802501895999488> Desarollador: [Wez#9777](https://discord.com/users/759233882926350346)
+> Desarollador: [Wez](https://discord.com/users/759233882926350346)
 
-> Languaje: <:JavaScript:1086800160039899277>  JavaScript
-> Libreria: <:DiscordJS:1086800106742874155>  Discord.JS v${require('discord.js').version}
-> Framework: <:Erine:1086800143170420766> Erine v${require('erine').version}
+> Languaje: JavaScript
+> Libreria: Discord.JS v${require('discord.js').version}
+> Framework: Erine v${require('erine').version}
 
-**<:link:1078064201454604369> Links**
+**Links**
 
-> <:GitHub:1087509695922716682> GitHub: [Click here](https://github.com/ImWezzz/Nara)
-> <:Invitame:1092644679541063761> Invite: [Click here](https://dsc.gg/nara-bot)
-> <:People:1087509665954406480> Server: [Click here](https://discord.gg/5FAvD6a92b)
+> GitHub: [Click here](https://github.com/ImWezzz/Nara)
+> Invite: [Click here](https://dsc.gg/nara-bot)
+> Server: [Click here](https://discord.gg/5FAvD6a92b)
 
 `)
       .setColor('dcdcdc')

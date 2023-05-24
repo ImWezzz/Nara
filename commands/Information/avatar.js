fdfc082d8dd92eff57652be5801f7c46fa15d1b4
ImWez <124311528+ImWezzz@users.noreply.github.com>
2023-05-24 01:24:06 -0300

    Create avatar.js

---
+++ commands/Information/avatar.js
@@ -0,0 +1,21 @@
+const { HybridBuilder, ParamsBuilder } = require('erine');
+const { EmbedBuilder } = require("discord.js");
+
+const body = {
+    data: new HybridBuilder()
+    .setName('avatar')
+    .setAliases('pfp')
+    .setDescription('Muestra el avatar de un usuario'),
+    params: new ParamsBuilder()
+    .addMember({ name: "user", description: "El usuario", required: false }),
+    async code(ctx) {
+    const u = ctx.get("user")?.user || ctx.author
+    const embed = new EmbedBuilder()
+        .setDescription(`Avatar de ${u.username}`)
+        .setImage(u.displayAvatarURL({ size: 1024 }))
+        .setColor('dcdcdc')
+    await ctx.send({ embeds: [embed] })
+    }
+}
+
+module.exports = { body }
